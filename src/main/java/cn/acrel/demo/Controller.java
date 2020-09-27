f49e55fb334f1b457d93b3847383d3311eb88feb
sxy <455624775@qq.com>
2020-09-27 22:09:43 +0800

    111

--- src/main/java/cn/acrel/demo/Controller.java
+++ src/main/java/cn/acrel/demo/Controller.java
@@ -4,13 +4,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
 import static cn.acrel.demo.ApiExample.*;
 
 @RestController
 public class Controller {
 
     @GetMapping(value = "/Get", produces = "application/json", consumes = "application/json")
-    public String get (){
+    public String get() {
         try {
             return httpGetExample("product");
         } catch (Exception e) {
@@ -20,14 +24,28 @@ public class Controller {
     }
 
     @PostMapping(value = "/Post", produces = "application/json", consumes = "application/json")
-    public String post(String produceId,String deviceId){
-        produceId="15000601";
-        deviceId="6b27bdb569094a6aa1849310be648149";
+    public Map<String, String> post(String produceId, String deviceId) {
+        produceId = "15000601";
+        deviceId = "6b27bdb569094a6aa1849310be648149";
+        Map<String,String> map=new HashMap<>();
         try {
-            return queryDeviceStatusList(produceId, deviceId);
+            String result = queryDeviceStatusList(produceId, deviceId);
+            int begin = result.indexOf("dataVal1");
+            result = result.substring(begin + 13, begin + 17);
+            String re = Test.hexStr2BinStr(result);
+            re = re.substring(11,15);
+            if (re.equals("0000"))
+                map.put("正常","没有报警");
+            if (re.equals("0001"))
+                map.put("报警","水浸报警");
+            if (re.equals("0100"))
+                map.put("报警","防拆报警");
+            if (re.equals("0101"))
+                map.put("报警","全部报警");
+            return map;
         } catch (Exception e) {
             e.printStackTrace();
         }
-        return null;
+        return map;
     }
 }
--- src/main/java/cn/acrel/demo/Test.java
+++ src/main/java/cn/acrel/demo/Test.java
@@ -1,5 +1,9 @@
 package cn.acrel.demo;
 
+import java.lang.reflect.Field;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Test {
     public static byte[] hexStringToBytes(String hexString) {
         if (hexString == null || hexString.equals("")) {
@@ -48,10 +52,60 @@ public class Test {
         return bytes;
 
     }
+    private static String hexStr =  "0123456789ABCDEF";
+    private static String[] binaryArray =
+            {"0000","0001","0010","0011",
+                    "0100","0101","0110","0111",
+                    "1000","1001","1010","1011",
+                    "1100","1101","1110","1111"};
+    /**
+     *
+     * @param hexString
+     * @return 将十六进制转换为二进制字节数组   16-2
+     */
+    public static byte[] hexStr2BinArr(String hexString){
+        //hexString的长度对2取整，作为bytes的长度
+        int len = hexString.length()/2;
+        byte[] bytes = new byte[len];
+        byte high = 0;//字节高四位
+        byte low = 0;//字节低四位
+        for(int i=0;i<len;i++){
+            //右移四位得到高位
+            high = (byte)((hexStr.indexOf(hexString.charAt(2*i)))<<4);
+            low = (byte)hexStr.indexOf(hexString.charAt(2*i+1));
+            bytes[i] = (byte) (high|low);//高地位做或运算
+        }
+        return bytes;
+    }
+    /**
+     *
+     * @param str
+     * @return 二进制数组转换为二进制字符串   2-2
+     */
+    public static String bytes2BinStr(byte[] bArray){
+
+        String outStr = "";
+        int pos = 0;
+        for(byte b:bArray){
+            //高四位
+            pos = (b&0xF0)>>4;
+            outStr+=binaryArray[pos];
+            //低四位
+            pos=b&0x0F;
+            outStr+=binaryArray[pos];
+        }
+        return outStr;
+    }
+    /**
+     *
+     * @param hexString
+     * @return 将十六进制转换为二进制字符串   16-2
+     */
+    public static String hexStr2BinStr(String hexString){
+        return bytes2BinStr(hexStr2BinArr(hexString));
+    }
 
     public static void main(String[] args) {
-        GetState getState=new GetState();
-      String result= getState.get("15000601","6b27bdb569094a6aa1849310be648149");
-        System.out.println(result);
+
     }
 }
