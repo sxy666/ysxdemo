21fb56caf09d7833c7286384bdc00e881f2e46cf
sxy <455624775@qq.com>
2020-10-19 14:26:26 +0800

    111

--- src/test/java/cn/acrel/demo/DemoApplicationTests.java
+++ src/test/java/cn/acrel/demo/DemoApplicationTests.java
@@ -3,7 +3,6 @@ package cn.acrel.demo;
 
 import cn.acrel.demo.entity.DeviceStateInfo;
 import cn.acrel.demo.service.QueryStateService;
-import cn.acrel.demo.va.PBKDF2Util;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -22,23 +21,6 @@ QueryStateService stateService;
 //       deviceStateInfo.setProduceID("1");
 //       deviceStateInfo.setDeviceID("1");
 //       updateState.updateSate(deviceStateInfo);
-
-
-        String password = "123456";
-
-        for (int i = 0; i < 5; i++) {
-            String salt = PBKDF2Util.generateSalt();
-            String pbkdf2 = PBKDF2Util.getEncryptedPassword(password,salt);
-
-            System.out.println("原始密码:"+password);
-            System.out.println("盐值:"+salt);
-            System.out.println("PBKDF2加盐后的密码:"+pbkdf2);
-            boolean authenticate = PBKDF2Util.authenticate("12346", pbkdf2, salt);
-            System.out.println(authenticate);
-            System.out.println("----------------------------");
-
-
-        }
 
 
     }
